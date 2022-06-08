3ac932e15e950c4b54249bae4f8a38cc31a97fb2
Luis Felipe de Paula Costa <lfbmrj15@gmail.com>
2022-06-08 16:51:01 -0300

    Contador html css js basico finalizado

---
+++ assets/js/scripts.js
@@ -0,0 +1,58 @@
+(() => {
+  const buttonIncrement = document.querySelector(".button-increment");
+  const buttonDecrement = document.querySelector(".button-decrement");
+
+  const currentNumber = document.querySelector(".current-number");
+  const keepGoingText = document.querySelector(".keep-going-text");
+
+  var counter = 0;
+
+  buttonIncrement.addEventListener("click", () => {
+    currentNumber.innerHTML = ++counter;
+    switch (counter) {
+      case 10:
+        keepGoingText.innerHTML = "Continue Voce esta quase la";
+        break;
+      case 20:
+        keepGoingText.innerHTML = "So mais um pouco!";
+        break;
+      case 40:
+        keepGoingText.innerHTML = "Quaseeeee!";
+        break;
+      case 80:
+        keepGoingText.innerHTML = "Bora que voce consegue!";
+        break;
+      case 100:
+        keepGoingText.innerHTML = "PARABEEEENS continue clicando!";
+        break;
+    }
+  });
+
+  buttonDecrement.addEventListener("click", () => {
+    if (counter <= 0) return;
+
+    counter -= 1;
+    currentNumber.innerHTML = counter;
+    switch (counter) {
+      case 0:
+        keepGoingText.innerHTML = "Tente contar para cima primeiro! do chao nao passa";
+        break;
+      case 10:
+        keepGoingText.innerHTML = "Assim voce ta quase perdendo";
+        break;
+      case 20:
+        keepGoingText.innerHTML = "Poxa voce estava quase la";
+        break;
+      case 40:
+        keepGoingText.innerHTML = "era Quaseeeee!";
+        break;
+      case 80:
+        keepGoingText.innerHTML =
+          "Bora que voce consegue! nao desista volte a clicar no +";
+        break;
+      case 100:
+        keepGoingText.innerHTML = "Percebeu que era golpe?";
+        break;
+    }
+  });
+})();
